f49fd81cfc33698e2f2e057bd04aa5d1b589742b
Haz-git <zhouharry1@gmail.com>
2021-01-04 10:39:25 -0800

    Added react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
+//Dependencies
 import React from 'react';
+import { Switch, Router, Route } from 'react-router-dom';
+import history from './historyObject';
+
+//Styling:
+import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+
+//Components:
+import MainLandingPage from './landingPage/MainLandingPage';
+import MainloginForm from './loginPage/MainloginForm';
+import MainSignupForm from './signupPage/MainSignupForm';
+import NavbarLanding from './landingPage/NavbarLanding';
+
+//Render
 
 const App = () => {
-    return <div>Initial Commit App Test with new master branch</div>;
+    return (
+        <>
+            <Router history={history}>
+                <NavbarLanding />
+                <Switch>
+                    <Route exact path="/" component={MainLandingPage} />
+                    <Route exact path="/login" component={MainloginForm} />
+                    <Route exact path="/signup" component={MainSignupForm} />
+                </Switch>
+            </Router>
+        </>
+    );
 };
 
 export default App;
