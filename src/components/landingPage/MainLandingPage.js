ac5a6a72267fbc52c87d5626ee9fd000323cbcbd
Haz-git <zhouharry1@gmail.com>
2021-01-04 10:38:09 -0800

    Added Basic templating

---
+++ src/components/landingPage/MainLandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLandingPage = () => {
+    return (
+        <>
+            <h1>This is the main landing page</h1>
+        </>
+    );
+};
+
+export default MainLandingPage;
