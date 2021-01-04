113f7706d3e2262ea7941477027183ac28b2022d
Haz-git <zhouharry1@gmail.com>
2021-01-04 10:41:40 -0800

    Created basic signupPage template

---
+++ src/components/signupPage/MainSignupForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainSignupForm = () => {
+    return (
+        <>
+            <h1>This is the sign up form page.</h1>
+        </>
+    );
+};
+
+export default MainSignupForm;
