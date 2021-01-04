b899f3719cf09dd8f293a954ef57b9fdc32cbab6
Haz-git <zhouharry1@gmail.com>
2021-01-04 10:40:08 -0800

    Added basic template from navbar

--- src/components/landingPage/NavbarLanding.js
+++ src/components/landingPage/NavbarLanding.js
@@ -1,1 +1,17 @@
 import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+
+const DefaultNavbar = styled.div``;
+
+//Render:
+const NavbarLanding = () => {
+    return (
+        <>
+            <p>This should be the navbar.</p>
+        </>
+    );
+};
+
+export default NavbarLanding;
