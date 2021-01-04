568c6a9784fcb9da63ebea62c9e1d544264144f4
Haz-git <zhouharry1@gmail.com>
2021-01-04 08:47:33 -0800

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-    return <div>Initial Commit App Test with dev</div>;
+    return <div>Initial Commit App Test with new master branch</div>;
 };
 
 export default App;
