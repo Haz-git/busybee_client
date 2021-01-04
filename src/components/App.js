707d532269126bd2a6c5bc012171376082c5f500
Haz-git <zhouharry1@gmail.com>
2021-01-04 11:56:02 -0800

    Introduced themes, globalstyle, and theme toggling functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 //Dependencies
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Router, Route } from 'react-router-dom';
 import history from './historyObject';
 
 //Styling:
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../styling/Theme';
+import { getMode } from '../styling/useDarkMode';
+import { GlobalStyle } from '../styling/GlobalStyles';
 
 //Components:
 import MainLandingPage from './landingPage/MainLandingPage';
@@ -16,18 +19,61 @@ import NavbarLanding from './landingPage/NavbarLanding';
 //Render
 
 const App = () => {
-    return (
-        <>
-            <Router history={history}>
-                <NavbarLanding />
-                <Switch>
-                    <Route exact path="/" component={MainLandingPage} />
-                    <Route exact path="/login" component={MainloginForm} />
-                    <Route exact path="/signup" component={MainSignupForm} />
-                </Switch>
-            </Router>
-        </>
-    );
+    const [appTheme, setAppTheme] = useState('');
+
+    useEffect(() => {
+        async function initialState() {
+            const initialModeValue = await getMode();
+            setAppTheme(initialModeValue);
+            console.log(appTheme);
+        }
+
+        initialState();
+    }, []);
+
+    const changeModeStatus = (modeValue) => {
+        //This callback function is passed down to UserSettings, where the toggle button is.
+        setAppTheme(modeValue);
+    };
+
+    const grabbedTheme =
+        appTheme === 'light' || appTheme === '' ? lightTheme : darkTheme;
+
+    const renderApp = () => {
+        if (appTheme === '') {
+            return null;
+        } else {
+            return (
+                <>
+                    <GlobalStyle />
+                    <ThemeProvider theme={grabbedTheme}>
+                        <Router history={history}>
+                            <NavbarLanding modeStatus={changeModeStatus} />
+                            <Switch>
+                                <Route
+                                    exact
+                                    path="/"
+                                    component={MainLandingPage}
+                                />
+                                <Route
+                                    exact
+                                    path="/login"
+                                    component={MainloginForm}
+                                />
+                                <Route
+                                    exact
+                                    path="/signup"
+                                    component={MainSignupForm}
+                                />
+                            </Switch>
+                        </Router>
+                    </ThemeProvider>
+                </>
+            );
+        }
+    };
+
+    return <>{renderApp()}</>;
 };
 
 export default App;
