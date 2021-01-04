f41d6e7b48b8f518484941d747dad2ae44060dc1
Haz-git <zhouharry1@gmail.com>
2021-01-04 10:41:00 -0800

    Created template for loginpage

---
+++ src/components/loginPage/MainloginForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainloginForm = () => {
+    return (
+        <>
+            <h1>This is the login form.</h1>
+        </>
+    );
+};
+
+export default MainloginForm;
