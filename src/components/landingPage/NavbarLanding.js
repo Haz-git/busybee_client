4cc1310eb4c234eb5db80341424cd6db299ccd97
Haz-git <zhouharry1@gmail.com>
2021-01-04 11:56:31 -0800

    Added toggler to navbar for testing purposes..?

--- src/components/landingPage/NavbarLanding.js
+++ src/components/landingPage/NavbarLanding.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import Toggler from '../../styling/Toggler';
+import { useDarkMode } from '../../styling/useDarkMode';
 
 //Styles:
 import styled from 'styled-components';
 
-const DefaultNavbar = styled.div``;
+const DefaultNavbar = styled.div`
+    background-color: ${(props) => props.theme.background};
+`;
 
 //Render:
-const NavbarLanding = () => {
+const NavbarLanding = ({ modeStatus }) => {
+    const [theme, toggleTheme] = useDarkMode();
+
     return (
-        <>
+        <DefaultNavbar>
             <p>This should be the navbar.</p>
-        </>
+            <div>
+                <Toggler
+                    theme={theme}
+                    toggleTheme={toggleTheme}
+                    callBack={modeStatus}
+                />
+            </div>
+        </DefaultNavbar>
     );
 };
 
