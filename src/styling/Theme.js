f16ffb9426b011a8d80b4dda4fb0e1737bdd6ba8
Haz-git <zhouharry1@gmail.com>
2021-01-04 11:57:51 -0800

    Added light and dark base themes. Will expand later

---
+++ src/styling/Theme.js
@@ -0,0 +1,11 @@
+//Light and Dark Themes:
+
+export const lightTheme = {
+    background: '#f6f9fc',
+    generalText: '#1c1e37',
+};
+
+export const darkTheme = {
+    background: '#293241',
+    generalText: 'f6f9fc',
+};
