a52a6eed6c2db6d2db1959cae0ebacc272bb3697
Haz-git <zhouharry1@gmail.com>
2021-01-04 14:09:13 -0800

    Added theme changes

--- src/styling/Theme.js
+++ src/styling/Theme.js
@@ -3,9 +3,11 @@
 export const lightTheme = {
     background: '#f6f9fc',
     generalText: '#1c1e37',
+    navBottomBorder: '#1c1e37',
 };
 
 export const darkTheme = {
     background: '#293241',
-    generalText: 'f6f9fc',
+    generalText: '#f6f9fc',
+    navBottomBorder: '#293241',
 };
