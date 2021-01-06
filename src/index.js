d2bce39c4bb6c0695d40e9a95baaa423018896ce
Haz-git <zhouharry1@gmail.com>
2021-01-06 10:15:38 -0800

    Attached redux store, redux-persist to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from '../src/redux/store';
 
 //Components:
 import App from './components/App';
 
 //Render:
 
-render(<App />, document.getElementById('root'));
+render(
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>,
+    document.getElementById('root')
+);
