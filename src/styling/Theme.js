21575900bd49ff4438f558e939eff5f84a9c65b6
Haz-git <zhouharry1@gmail.com>
2021-01-06 15:17:32 -0800

    minor theme changes

--- src/styling/Theme.js
+++ src/styling/Theme.js
@@ -1,13 +1,17 @@
 //Light and Dark Themes:
 
 export const lightTheme = {
+    //Generals:
     background: '#f6f9fc',
     generalText: '#1c1e37',
     navBottomBorder: '#1c1e37',
+    //Sign up:
 };
 
 export const darkTheme = {
+    //Generals:
     background: '#293241',
     generalText: '#f6f9fc',
     navBottomBorder: '#293241',
+    //Sign up:
 };
