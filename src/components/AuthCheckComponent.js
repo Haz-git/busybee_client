85f511b9f52cd8965c26fccffbb9938625cd4891
Haz-git <zhouharry1@gmail.com>
2021-01-07 10:56:38 -0800

    Auth checker component updated with utility to check if JWT has expired.

--- src/components/AuthCheckComponent.js
+++ src/components/AuthCheckComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getJWT } from '../utils/jwthelper';
 import history from './historyObject';
+import jsonwebtoken from 'jsonwebtoken';
 
 class AuthenticatedComponents extends Component {
     constructor(props) {
@@ -13,11 +14,27 @@ class AuthenticatedComponents extends Component {
 
     componentDidMount() {
         // Use helper function to grab JWT from localstorage.
+        let isExpired = false;
         const jwt = getJWT();
 
         //If there's no JWT present in the localstorage, no JWT must be sent over from server, meaning that login is wrong. Push the user back to the login page.
         if (!jwt) {
             history.push('/login');
+        } else {
+            //After checking if there is a JWT in the first place, we must check if the JWT is expired.
+            const jsonWebToken = localStorage.getItem('jwt');
+            const formattedToken = jsonWebToken.split(' ')[1].slice(0, -1);
+            const decodedToken = jsonwebtoken.decode(formattedToken, {
+                complete: true,
+            });
+
+            const dateNow = new Date();
+
+            if (decodedToken.payload.exp * 1000 < dateNow.getTime()) {
+                isExpired = true;
+                history.push('/login');
+                alert('Your session has expired. Please log in to continue.');
+            }
         }
 
         //If the user's JWT is present: set the React state to 'temp' instead of undefined.
