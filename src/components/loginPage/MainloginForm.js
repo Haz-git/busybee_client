7ef320a6cc7e6315a88a1aa8c125b6f1e7d7b373
Haz-git <zhouharry1@gmail.com>
2021-01-07 09:36:47 -0800

    Added basic login form

--- src/components/loginPage/MainloginForm.js
+++ src/components/loginPage/MainloginForm.js
@@ -1,16 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { reduxForm } from 'redux-form';
+import userLogin from '../../redux/userLogin/userLoginActions';
+import InputField from '../signupPage/InputField';
 
-const MainloginForm = () => {
+//Styles:
+import styled from 'styled-components';
+import SubmitButton from '../signupPage/SubmitButton';
+
+const MainContainer = styled.div``;
+
+const WrapperContainer = styled.div``;
+
+const FormContainer = styled.div``;
+
+//Render:
+const MainLoginForm = ({ handleSubmit, userLogin }) => {
+    const handleUserLogin = (formValues) => {
+        userLogin(formValues);
+    };
+
     return (
         <>
-            <h1>This is the login form.</h1>
-            <div>
-                <Link to="/">Main Page Link</Link>
-                <Link to="/signup">Registration form link</Link>
-            </div>
+            <MainContainer>
+                <WrapperContainer>
+                    <h1>This is the login form.</h1>
+                    <form onSubmit={handleSubmit(handleUserLogin)}>
+                        <FormContainer>
+                            <InputField
+                                formName="email"
+                                componentType="input"
+                                label="Email Address"
+                                htmlType="email"
+                            />
+                            <InputField
+                                formName="password"
+                                componentType="input"
+                                label="Password"
+                                htmlType="password"
+                            />
+                        </FormContainer>
+                        <div>
+                            <SubmitButton label="Login" />
+                        </div>
+                    </form>
+                    <div>
+                        <Link to="/">Main Page Link</Link>
+                        <Link to="/signup">Registration form link</Link>
+                    </div>
+                </WrapperContainer>
+            </MainContainer>
         </>
     );
 };
 
-export default MainloginForm;
+//Connecting to Redux and Redux-form:
+
+const reduxConnect = connect(null, { userLogin })(MainLoginForm);
+
+export default reduxForm({
+    form: 'loginForm',
+})(reduxConnect);
