8f61dcfd0d0077d8abbad24f4233adbe7f0640fc
Haz-git <zhouharry1@gmail.com>
2021-01-07 08:24:09 -0800

    Added minor themes

--- src/styling/Theme.js
+++ src/styling/Theme.js
@@ -5,6 +5,9 @@ export const lightTheme = {
     background: '#f6f9fc',
     generalText: '#1c1e37',
     navBottomBorder: '#1c1e37',
+    //MainMobilePage:
+    mainMobBackground: '#335c67',
+    mainMobHeaderColor: '#1c1e37',
     //Sign up:
 };
 
@@ -13,5 +16,8 @@ export const darkTheme = {
     background: '#293241',
     generalText: '#f6f9fc',
     navBottomBorder: '#293241',
+    //MainMobilePage:
+    mainMobBackground: '#0d1b2a',
+    mainMobHeaderColor: '#e0e1dd',
     //Sign up:
 };
