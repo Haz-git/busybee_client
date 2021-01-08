f2ab79fb443f55b68c8e43b3990731bb401fe1b6
Haz-git <zhouharry1@gmail.com>
2021-01-08 10:02:06 -0800

    Added more theme colors

--- src/styling/Theme.js
+++ src/styling/Theme.js
@@ -5,10 +5,18 @@ export const lightTheme = {
     background: '#f6f9fc',
     generalText: '#1c1e37',
     navBottomBorder: '#1c1e37',
+    inputFieldLabel: 'black',
+    inputFieldColor: 'black',
+    inputFieldBG: 'white',
     //MainMobilePage:
     mainMobBackground: '#335c67',
     mainMobHeaderColor: '#1c1e37',
     //Sign up:
+    MobIconColor: 'black',
+    MobPromptSpan: 'black',
+    MobPromptLink: '#14A84B',
+    //Login
+    MobHeaderColor: 'black',
 };
 
 export const darkTheme = {
@@ -16,8 +24,16 @@ export const darkTheme = {
     background: '#293241',
     generalText: '#f6f9fc',
     navBottomBorder: '#293241',
+    inputFieldLabel: '#E8E8E8',
+    inputFieldColor: 'white',
+    inputFieldBG: '#1c1e37',
     //MainMobilePage:
     mainMobBackground: '#0d1b2a',
     mainMobHeaderColor: '#e0e1dd',
     //Sign up:
+    MobIconColor: 'white',
+    MobPromptSpan: 'white',
+    MobPromptLink: '#14A84B',
+    //Login
+    MobHeaderColor: '#E8E8E8',
 };
