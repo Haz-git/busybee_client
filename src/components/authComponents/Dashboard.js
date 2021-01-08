a96968ba7b6cab6c3c29d8f66ed03cb76982a7c2
Haz-git <zhouharry1@gmail.com>
2021-01-08 15:38:45 -0800

    Created dashboard

--- src/components/authComponents/Dashboard.js
+++ src/components/authComponents/Dashboard.js
@@ -5,7 +5,7 @@ const Dashboard = () => {
         <>
             <h1>
                 This should be a protected component, it should not reveal
-                unless you have a JWT.
+                unless you have a JWT. This is the main dashboard.
             </h1>
         </>
     );
