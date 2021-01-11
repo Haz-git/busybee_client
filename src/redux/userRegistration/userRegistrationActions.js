7b8fac2e81b20633f1b80230a34e0ca73e34c05a
Haz-git <zhouharry1@gmail.com>
2021-01-11 10:36:07 -0800

    Removed push to login, set up callback to reveal snackbar

--- src/redux/userRegistration/userRegistrationActions.js
+++ src/redux/userRegistration/userRegistrationActions.js
@@ -2,18 +2,23 @@ import api from '../../api';
 import history from '../../components/historyObject';
 import { USER_REGISTRATION } from './userRegistrationTypes';
 
-const userRegistration = (formValues) => async (dispatch) => {
+const userRegistration = (formValues, callback) => async (dispatch) => {
     //Sending redux-form values to Atlas:
 
     const response = await api.post('/user/signup', { ...formValues });
 
     //We'll feint a dispatch for the time being. There is no reducer set to collect this data.
-    dispatch({
-        type: USER_REGISTRATION,
-        payload: response.data,
-    });
+    // dispatch({
+    //     type: USER_REGISTRATION,
+    //     payload: response.data,
+    // });
+    if (response) {
+        console.log(response);
+        //if response.ok === true, then the user's account has been created.
+        callback(true);
+    }
 
-    history.push('/login');
+    // history.push('/login');
 };
 
 export default userRegistration;
