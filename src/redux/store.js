20758fe9b11172c86f3157289b12dbd516f50ea5
Haz-git <zhouharry1@gmail.com>
2021-01-12 11:37:16 -0800

    Removed userdetails

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,7 +14,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['auth'],
+    whitelist: ['auth', 'user'],
 };
 
 //Creating enhancers:
