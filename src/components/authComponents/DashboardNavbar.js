37a86d42fc773ba27f99b86ef7c7b4718907e4aa
Haz-git <zhouharry1@gmail.com>
2021-01-12 11:36:39 -0800

    Created rudimentary bottom navbar

--- src/components/authComponents/DashboardNavbar.js
+++ src/components/authComponents/DashboardNavbar.js
@@ -1,17 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //Styles:
 import styled from 'styled-components';
+//Icons:
+import { Home } from '@styled-icons/ionicons-solid/Home';
+import { Calendar } from '@styled-icons/boxicons-regular/Calendar';
+import { StatsChart } from '@styled-icons/ionicons-solid/StatsChart';
+import { UserCog } from '@styled-icons/fa-solid/UserCog';
+import { Barbell } from '@styled-icons/ionicons-outline/Barbell';
+
+const StyledHomeIcon = styled(Home)`
+    height: 2em;
+    width: 2em;
+    color: ${({ theme }) => theme.NavIconColor};
+    cursor: pointer;
+`;
+
+const StyledCalendarIcon = styled(Calendar)`
+    height: 2em;
+    width: 2em;
+    color: ${({ theme }) => theme.NavIconColor};
+    cursor: pointer;
+`;
+
+const StyledStatsIcon = styled(StatsChart)`
+    height: 2em;
+    width: 2em;
+    color: ${({ theme }) => theme.NavIconColor};
+    cursor: pointer;
+`;
+
+const StyledUserCogIcon = styled(UserCog)`
+    height: 2em;
+    width: 2em;
+    color: ${({ theme }) => theme.NavIconColor};
+    cursor: pointer;
+`;
+
+const StyledBarbellIcon = styled(Barbell)`
+    height: 2em;
+    width: 2em;
+    color: ${({ theme }) => theme.NavIconColor};
+    cursor: pointer;
+`;
 
 const MainContainer = styled.div`
-    background-color: salmon;
+    background: ${({ theme }) => theme.NavColor};
     overflow: hidden;
     position: fixed;
     bottom: 0;
     width: 100%;
 `;
 
-const NavItemContainer = styled.div``;
+const NavItemContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+const NavItem = styled(Link)`
+    padding: 0.8em 0.8em;
+`;
 
 //Render:
 
@@ -19,7 +69,23 @@ const DashboardNavbar = () => {
     return (
         <>
             <MainContainer>
-                <NavItemContainer>Bottom navbar components</NavItemContainer>
+                <NavItemContainer>
+                    <NavItem>
+                        <StyledCalendarIcon />
+                    </NavItem>
+                    <NavItem>
+                        <StyledStatsIcon />
+                    </NavItem>
+                    <NavItem>
+                        <StyledHomeIcon />
+                    </NavItem>
+                    <NavItem>
+                        <StyledBarbellIcon />
+                    </NavItem>
+                    <NavItem>
+                        <StyledUserCogIcon />
+                    </NavItem>
+                </NavItemContainer>
             </MainContainer>
         </>
     );
