9a40d714a660ce55506f6e0675535151dfd82178
Haz-git <zhouharry1@gmail.com>
2021-01-13 08:48:50 -0800

    Created new block for main dashboard

--- src/components/authComponents/dashboardComponents/UserPowerStats.js
+++ src/components/authComponents/dashboardComponents/UserPowerStats.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
+//Components:
+import UserPowerStatCard from './UserPowerStatCard';
+
+//Imgs:
+import benchpress from '../../../imgs/benchpress256.png';
+import squat from '../../../imgs/squat256.png';
+import deadlift from '../../../imgs/deadlift256.png';
+
 //Styles:
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
+    margin-top: 1em;
     text-align: left;
 `;
 
 const MainHeader = styled.h2`
     font-family: 'Lato';
-    font-size: 0.85em;
+    font-size: 0.9em;
     color: ${({ theme }) => theme.UserPowerHeaderColor};
-    font-weight: 400;
+    font-weight: 900;
+`;
+
+const StatCardContainer = styled.div`
+    margin-top: 1em;
+    display: flex;
 `;
 
 //Render:
@@ -20,7 +34,12 @@ const UserPowerStats = () => {
     return (
         <>
             <MainContainer>
-                <MainHeader>Your progress</MainHeader>
+                <MainHeader>Your main lifts</MainHeader>
+                <StatCardContainer>
+                    <UserPowerStatCard header="Deadlift" img={deadlift} />
+                    <UserPowerStatCard header="Squat" img={squat} />
+                    <UserPowerStatCard header="Bench" img={benchpress} />
+                </StatCardContainer>
             </MainContainer>
         </>
     );
