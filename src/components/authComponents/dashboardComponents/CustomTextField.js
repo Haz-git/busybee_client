1ad34f727daaf986d5eba80515f5916429adf4a6
Haz-git <zhouharry1@gmail.com>
2021-01-15 15:24:16 -0800

    Created text field for modal

---
+++ src/components/authComponents/dashboardComponents/CustomTextField.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+//Styles:
+const StyledTextField = styled.input`
+    margin: 0.5em 0.5em;
+    border: 1px solid white;
+    border-radius: 5em;
+    background-color: inherit;
+    color: white;
+    padding: 0.5em 1em;
+    width: 10em;
+    font-family: 'Nunito', sans-serif, helvetica;
+    font-weight: 900;
+
+    &:hover {
+        outline: none;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+//Render:
+
+const CustomTextField = ({ type, placeholder, existingStat, changeFunc }) => {
+    return (
+        <StyledTextField
+            type={type}
+            placeholder={existingStat}
+            onChange={changeFunc}
+        />
+    );
+};
+
+export default CustomTextField;
