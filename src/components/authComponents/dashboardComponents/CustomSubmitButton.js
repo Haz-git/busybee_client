20445fea581e5f9fdad3fe385309fc472214fabf
Haz-git <zhouharry1@gmail.com>
2021-01-15 15:23:54 -0800

    Created custom submit button for modal

---
+++ src/components/authComponents/dashboardComponents/CustomSubmitButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+
+//Styles:
+
+const StyledSubmitButton = withStyles({
+    root: {
+        borderRadius: '2em',
+        minWidth: '100%',
+        maxWidth: '100%',
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+        background:
+            'linear-gradient(90deg, rgba(87,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
+        color: 'white',
+        '&:hover': {
+            background:
+                'linear-gradient(90deg, rgba(87,97,0,1) 0%, rgba(0,133,46,1) 50%, rgba(0,133,150,1) 100%)',
+        },
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
+})(Button);
+
+const CustomSubmitButton = ({ variant, label, type }) => {
+    return (
+        <StyledSubmitButton variant={variant} type={type}>
+            {label}
+        </StyledSubmitButton>
+    );
+};
+
+export default CustomSubmitButton;
