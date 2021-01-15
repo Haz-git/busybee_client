2abb2b5adaa353c439f3dfe970e6e0f05e51fa41
Haz-git <zhouharry1@gmail.com>
2021-01-15 15:23:08 -0800

    Custom selector created for module

---
+++ src/components/authComponents/dashboardComponents/CustomSelector.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+//Styles:
+
+const StyledSelector = styled.select`
+    margin: 0.5em 0.5em;
+    border: 1px solid white;
+    border-radius: 5em;
+    background-color: inherit;
+    color: white;
+    padding: 0.5em 1em;
+    font-family: 'Nunito', sans-serif, helvetica;
+    font-weight: 900;
+
+    &:hover {
+        outline: none;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+const StyledOption = styled.option``;
+//Render:
+
+const CustomSelector = ({ changeFunc }) => {
+    return (
+        <StyledSelector onChange={changeFunc}>
+            <StyledOption value="Lbs">Lbs</StyledOption>
+            <StyledOption value="Kgs">Kgs</StyledOption>
+        </StyledSelector>
+    );
+};
+
+export default CustomSelector;
