e055cac00e4c8ac1d64b6bf0b540f1d48f72e81c
Haz-git <zhouharry1@gmail.com>
2021-01-17 07:40:11 -0800

    added page for programming

---
+++ src/components/authComponents/programDashboard/MainPrograms.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+
+//Render:
+const MainPrograms = () => {
+    return (
+        <>
+            <h2>This should be the main programs page.</h2>
+        </>
+    );
+};
+
+export default MainPrograms;
