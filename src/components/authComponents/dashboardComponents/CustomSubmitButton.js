3874b28a0d1919e657983a6e321909af7cd72cf6
Haz-git <zhouharry1@gmail.com>
2021-01-18 06:42:36 -0800

    Minor padding changes

--- src/components/authComponents/dashboardComponents/CustomSubmitButton.js
+++ src/components/authComponents/dashboardComponents/CustomSubmitButton.js
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 
 const StyledSubmitButton = withStyles({
     root: {
+        fontSize: '.8em',
         borderRadius: '2em',
         minWidth: '100%',
         maxWidth: '100%',
@@ -14,6 +15,7 @@ const StyledSubmitButton = withStyles({
         background:
             'linear-gradient(90deg, rgba(87,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
         color: 'white',
+        padding: '.45em .4em',
         '&:hover': {
             background:
                 'linear-gradient(90deg, rgba(87,97,0,1) 0%, rgba(0,133,46,1) 50%, rgba(0,133,150,1) 100%)',
