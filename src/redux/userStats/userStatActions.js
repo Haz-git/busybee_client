2d5953ac70363f4165ddef66bb96bd6e3c84256e
Haz-git <zhouharry1@gmail.com>
2021-01-20 08:48:16 -0800

    Created deletion and adding function

--- src/redux/userStats/userStatActions.js
+++ src/redux/userStats/userStatActions.js
@@ -14,10 +14,34 @@ export function getUserStatData() {
     return async (dispatch, getState) => {
         const response = await api.get('/user/getallstats');
 
-        console.log(response);
-
         dispatch({
             type: USER_RETRIEVE_STATS,
+            payload: response.data.userSavedStats,
+        });
+    };
+}
+
+export function addNewStat(exerciseName) {
+    return async (dispatch) => {
+        const response = await api.post('/user/addnewstat', { exerciseName });
+
+        dispatch({
+            type: USER_ADD_NEW_STAT,
+            payload: response.data.userSavedStats,
+        });
+    };
+}
+
+export function deleteStat(exerciseId) {
+    return async (dispatch) => {
+        //For delete axios requests, data is possible in request bodies, but the following format is required.
+
+        const response = await api.delete('/user/deletestat', {
+            data: { exerciseId },
+        });
+
+        dispatch({
+            type: USER_ADD_NEW_STAT,
             payload: response.data.userSavedStats,
         });
     };
