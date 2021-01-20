04e39a51dad0a08029676a5bc4e622eb87005ae7
Haz-git <zhouharry1@gmail.com>
2021-01-20 11:00:24 -0800

    Created edit modal

---
+++ src/components/authComponents/statsDashboard/StatCardModalEdit.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import CustomTextField from '../dashboardComponents/CustomTextField';
+import { withStyles } from '@material-ui/core/styles';
+
+import {
+    ModalContainer,
+    ModalHeader,
+    ModalDesc,
+} from '../dashboardComponents/UserPowerStatCard';
+
+const DeleteModalContainer = styled(ModalContainer)`
+    width: 82.5%;
+`;
+
+const DeleteModalHeader = styled(ModalHeader)`
+    white-space: normal;
+`;
+
+const InputContainer = styled.div`
+    margin: 0.6em 0;
+`;
+
+const DeleteModalDesc = styled(ModalDesc)`
+    white-space: normal;
+    text-align: justify;
+    text-justify: distribute;
+    text-align-last: left;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+const StyledButton = withStyles({
+    root: {
+        textTransform: 'capitalize',
+    },
+})(Button);
+
+const StatCardModalDelete = ({
+    openBoolean,
+    closeFunction,
+    buttonSubmitFunction,
+    inputFunction,
+}) => {
+    return (
+        <>
+            <Modal
+                aria-labelledby="stat card edit modal"
+                aria-describedby="modal for stat edit"
+                open={openBoolean}
+                onClose={closeFunction}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openBoolean}>
+                    <DeleteModalContainer>
+                        <DeleteModalHeader>Edit Stat Name</DeleteModalHeader>
+                        <InputContainer>
+                            <CustomTextField
+                                type="text"
+                                placeholder="New name..."
+                                changeFunc={inputFunction}
+                                maxlength={17}
+                            />
+                        </InputContainer>
+                        <ButtonContainer>
+                            <StyledButton
+                                size="small"
+                                variant="contained"
+                                color="primary"
+                                onClick={buttonSubmitFunction}
+                            >
+                                Save
+                            </StyledButton>
+                            <StyledButton
+                                size="small"
+                                variant="contained"
+                                onClick={closeFunction}
+                            >
+                                Cancel
+                            </StyledButton>
+                        </ButtonContainer>
+                    </DeleteModalContainer>
+                </Fade>
+            </Modal>
+        </>
+    );
+};
+
+export default StatCardModalDelete;
