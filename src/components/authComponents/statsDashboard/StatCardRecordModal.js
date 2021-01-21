2fa7009130b384e70040ca8bef0c616817555cac
Haz-git <zhouharry1@gmail.com>
2021-01-21 10:38:40 -0800

    Created modal for entering new records

--- src/components/authComponents/statsDashboard/StatCardRecordModal.js
+++ src/components/authComponents/statsDashboard/StatCardRecordModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Styles:
 import styled from 'styled-components';
@@ -9,6 +9,10 @@ import Button from '@material-ui/core/Button';
 import CustomTextField from '../dashboardComponents/CustomTextField';
 import { withStyles } from '@material-ui/core/styles';
 import RecordCard from './RecordCard';
+import RecordCardAddModal from './RecordCardAddModal';
+import { connect } from 'react-redux';
+import { addRecord } from '../../../redux/userStats/userStatActions';
+import { v4 as uuid } from 'uuid';
 
 import {
     ModalContainer,
@@ -27,7 +31,7 @@ const RecordModalContainer = styled.div`
     transform: translate(-50%, -50%);
     position: relative;
     background: ${({ theme }) => theme.ModalBG};
-    /* border: 1px solid #fdbc3d; */
+    border: 1px solid #fdbc3d;
     border-radius: 0.4em;
     padding: 1em 1em;
     display: flex;
@@ -74,11 +78,22 @@ const StatCardRecordModal = ({
     buttonSubmitFunction,
     inputFunction,
     recordArray,
+    addRecord,
+    exerciseId,
 }) => {
+    //Controls state of modal to add new record:
+
+    const [stateAddRecordModal, setStateAddRecordModal] = useState(false);
+    const [weightInput, setWeightInput] = useState(null);
+    const [setsInput, setSetsInput] = useState(null);
+    const [repsInput, setRepsInput] = useState(null);
+    const [unitSelect, setUnitSelect] = useState(null);
+
     const renderRecordCards = () => {
         if (recordArray !== undefined && recordArray !== null) {
             return recordArray.map((record) => (
                 <RecordCard
+                    key={uuid()}
                     sets={record.sets}
                     reps={record.reps}
                     weight={record.weight}
@@ -88,6 +103,39 @@ const StatCardRecordModal = ({
         } else {
             return <>You don't seem to have any records!</>;
         }
+    };
+
+    //Controller functions for modal to add a new record:
+
+    const openAddRecordModal = () => {
+        setStateAddRecordModal(true);
+    };
+
+    const closeAddRecordModal = () => {
+        setStateAddRecordModal(false);
+    };
+
+    const handleWeightChange = (e) => {
+        setWeightInput(e.target.value);
+    };
+
+    const handleRepsChange = (e) => {
+        setRepsInput(e.target.value);
+    };
+
+    const handleSetsChange = (e) => {
+        setSetsInput(e.target.value);
+    };
+
+    const handleUnitSelect = (e) => {
+        setUnitSelect(e.target.value);
+    };
+
+    const handleSubmission = (e) => {
+        e.preventDefault();
+
+        addRecord(exerciseId, setsInput, repsInput, weightInput, unitSelect);
+        setStateAddRecordModal(false);
     };
 
     return (
@@ -115,6 +163,7 @@ const StatCardRecordModal = ({
                                 variant="contained"
                                 color="primary"
                                 startIcon={<AddIcon />}
+                                onClick={openAddRecordModal}
                             >
                                 Add New Record
                             </StyledButton>
@@ -130,8 +179,17 @@ const StatCardRecordModal = ({
                     </RecordModalContainer>
                 </Fade>
             </Modal>
+            <RecordCardAddModal
+                openBoolean={stateAddRecordModal}
+                closeFunction={closeAddRecordModal}
+                weightFunction={handleWeightChange}
+                setsFunction={handleSetsChange}
+                repsFunction={handleRepsChange}
+                unitFunction={handleUnitSelect}
+                submitHandler={handleSubmission}
+            />
         </>
     );
 };
 
-export default StatCardRecordModal;
+export default connect(null, { addRecord })(StatCardRecordModal);
