76ea1346a57a262bbf2c65e329b31c592622fe08
Haz-git <zhouharry1@gmail.com>
2021-01-22 08:18:10 -0800

    Styled record card

--- src/components/authComponents/statsDashboard/RecordCard.js
+++ src/components/authComponents/statsDashboard/RecordCard.js
@@ -3,15 +3,120 @@ import dayjs from 'dayjs';
 
 //Styles:
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { Cancel } from '@styled-icons/material-rounded/Cancel';
+import { Pencil } from '@styled-icons/remix-fill/Pencil';
 
 const MainContainer = styled.div`
     display: flex;
-    padding: 0.5em 0.5em;
-    margin: 0.5em 0;
-    background: navy;
+    position: relative;
+    margin-bottom: 0.5em;
+    background: #273a5a;
+    border-radius: 0.2em;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 3px 8px;
+    padding: 0.5em 0;
 `;
 
-const DetailsContainer = styled.div``;
+const EditIcon = styled(Pencil)`
+    width: 1.35em;
+    height: 1.35em;
+    color: ${({ theme }) => theme.EditIcon};
+`;
+
+const DeleteIcon = styled(Cancel)`
+    width: 1.35em;
+    height: 1.35em;
+    color: ${({ theme }) => theme.TrashIcon};
+`;
+
+const FlexContainer = styled.div`
+    align-items: center;
+    display: flex;
+`;
+
+const DetailsContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 0.5em 0em;
+    text-align: left;
+`;
+
+const DateContainer = styled.div`
+    text-align: left;
+    padding-top: 0.2em;
+    padding-bottom: 0.2em;
+    padding-left: 0.5em;
+`;
+
+const DateText = styled.h2`
+    font-size: 0.7em;
+    font-weight: 900;
+    color: #e6ac00;
+`;
+
+const WeightContainer = styled.div`
+    white-space: nowrap;
+    margin: 0 1em;
+    font-size: 0.7em;
+    font-weight: 900;
+    color: ${({ theme }) => theme.generalText};
+`;
+
+const WeightDivider = styled.div``;
+
+const TechContainer = styled.div`
+    white-space: nowrap;
+    margin: 0 1em;
+    font-size: 0.7em;
+    font-weight: 900;
+    color: ${({ theme }) => theme.generalText};
+`;
+
+const TechDivider = styled.div``;
+
+const EditButton = styled.button`
+    background: #1c1e37;
+    cursor: pointer;
+    border: none;
+    padding: 0.5em 0.5em;
+    border-top-right-radius: 0.2em;
+
+    &:hover {
+        outline: none;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+const DeleteButton = styled.button`
+    background: #1c1e37;
+    cursor: pointer;
+    border: none;
+    padding: 0.5em 0.5em;
+    border-bottom-right-radius: 0.2em;
+
+    &:hover {
+        outline: none;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    border-bottom-right-radius: 0.2em;
+    border-top-right-radius: 0.2em;
+`;
 
 //Render:
 
@@ -27,8 +132,32 @@ const RecordCard = ({ sets, reps, weight, dateModified }) => {
     return (
         <>
             <MainContainer>
-                Sets: {sets}, Reps: {reps}, {weight} Lbs, {convertWeightToKg()}{' '}
-                Kgs, Date Modified: {convertISOToDate()}
+                <FlexContainer>
+                    <DateContainer>
+                        <DateText>Edited on:</DateText>
+                        <DateText>{convertISOToDate()}</DateText>
+                        <ButtonContainer>
+                            <EditButton>
+                                <EditIcon />
+                            </EditButton>
+                            <DeleteButton>
+                                <DeleteIcon />
+                            </DeleteButton>
+                        </ButtonContainer>
+                    </DateContainer>
+                    <DetailsContainer>
+                        <TechContainer>
+                            <TechDivider>Sets: {sets}</TechDivider>
+                            <TechDivider>Reps: {reps}</TechDivider>
+                        </TechContainer>
+                        <WeightContainer>
+                            <WeightDivider>{weight} Lbs</WeightDivider>
+                            <WeightDivider>
+                                {convertWeightToKg()} Kgs
+                            </WeightDivider>
+                        </WeightContainer>
+                    </DetailsContainer>
+                </FlexContainer>
             </MainContainer>
         </>
     );
