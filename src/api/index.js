ef0d975d78b96079cbcfff539ee54fc9acb2bbcf
Haz-git <zhouharry1@gmail.com>
2021-01-23 07:42:13 -0800

    prod

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 //This will be the connection to our API hosted temporarily on localhost. After preliminary setup, the API will be hosted on Heroku.
 
 export default axios.create({
-    baseURL: 'http://localhost:7467/api',
-    // baseURL: 'https://gymjot.herokuapp.com/api',
+    // baseURL: 'http://localhost:7467/api',
+    baseURL: 'https://gymjot.herokuapp.com/api',
     withCredentials: true,
     headers: {
         Authorization: JSON.parse(localStorage.getItem('jwt')),
