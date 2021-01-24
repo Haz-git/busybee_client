b171c47f42de52cccacb8013fb2e712816ae5951
Haz-git <zhouharry1@gmail.com>
2021-01-24 07:32:52 -0800

    Added program manager placeholder

--- src/components/authComponents/programDashboard/MainPrograms.js
+++ src/components/authComponents/programDashboard/MainPrograms.js
@@ -2,12 +2,32 @@ import React from 'react';
 
 //Styles:
 import styled from 'styled-components';
+import { MainHeader } from '../dashboardComponents/UserGreeting';
+
+const MainContainer = styled.div`
+    display: block;
+    text-align: center;
+    padding: 0.5em 0.5em;
+    /* overflow-y: scroll; */
+`;
+
+const SecondaryProgramHeader = styled(MainHeader)`
+    font-size: 1em;
+    font-weight: 400;
+    white-space: nowrap;
+    margin: 0.7em 0;
+`;
 
 //Render:
 const MainPrograms = () => {
     return (
         <>
-            <h2>This should be the main programs page.</h2>
+            <MainContainer>
+                <MainHeader>Program Manager</MainHeader>
+                <SecondaryProgramHeader>
+                    Design and run your lifting programs.
+                </SecondaryProgramHeader>
+            </MainContainer>
         </>
     );
 };
