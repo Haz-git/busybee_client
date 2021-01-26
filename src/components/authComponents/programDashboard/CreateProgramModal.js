1226bacfbc04d032760412633ef36524206b364a
Haz-git <zhouharry1@gmail.com>
2021-01-26 09:29:14 -0800

    Added input text fields to modals

--- src/components/authComponents/programDashboard/CreateProgramModal.js
+++ src/components/authComponents/programDashboard/CreateProgramModal.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import GeneralSaveButton from './GeneralSaveButton';
+import GeneralCancelButton from './GeneralCancelButton';
 
 //Styles:
 import styled from 'styled-components';
@@ -14,12 +16,94 @@ import {
 
 const CreateProgramModalContainer = styled(ModalContainer)`
     width: 92%;
+    position: absolute;
+    left: 50%;
+    top: 40%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
 `;
 
 const CreateProgramModalHeader = styled(ModalHeader)`
-    font-size: 1.2em;
+    font-size: 1.4em;
     font-weight: 900;
-    margin: 0.5em 0;
+    margin: 0.4em 0;
+`;
+
+const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1em;
+`;
+
+const InputDivider = styled.div`
+    margin: 0.6em 0;
+`;
+
+const TitleInput = styled.input`
+    margin: 0 auto;
+    /* margin: 0.5em 0.5em; */
+    border: 1px solid white;
+    border-radius: 0.3em;
+    background-color: inherit;
+    color: white;
+    padding: 0.5em 1em;
+    width: 100%;
+    font-family: 'Nunito', sans-serif, helvetica;
+    font-weight: 900;
+    font-size: 1em;
+
+    &:hover {
+        outline: none;
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    @media only screen and (min-width: 375px) {
+        font-size: 1.3em;
+    }
+`;
+
+const DescLabel = styled.label``;
+
+const DescInput = styled.textarea`
+    margin: 0 auto;
+    border: 1px solid white;
+    border-radius: 0.3em;
+    background-color: inherit;
+    color: white;
+    padding: 0.5em 1em;
+    width: 100%;
+    height: 10em;
+    font-family: 'Nunito', sans-serif, helvetica;
+    font-weight: 300;
+    font-size: 0.7em;
+
+    &:hover {
+        outline: none;
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    @media only screen and (min-width: 375px) {
+        font-size: 1.3em;
+    }
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const ButtonMargin = styled.span`
+    margin: 0 2em;
 `;
 
 //Render:
@@ -49,6 +133,32 @@ const CreateProgramModal = ({
                         <CreateProgramModalHeader>
                             Create a new Program
                         </CreateProgramModalHeader>
+                        <InputContainer>
+                            <InputDivider>
+                                <TitleInput
+                                    placeholder="Program name..."
+                                    onChange={titleFunction}
+                                />
+                            </InputDivider>
+                            <InputDivider>
+                                <DescInput
+                                    placeholder="Description (optional)..."
+                                    onChange={descFunction}
+                                />
+                            </InputDivider>
+                            <ButtonContainer>
+                                <ButtonMargin>
+                                    <GeneralSaveButton
+                                        clickFunction={submitHandler}
+                                    />
+                                </ButtonMargin>
+                                <ButtonMargin>
+                                    <GeneralCancelButton
+                                        clickFunction={closeFunction}
+                                    />
+                                </ButtonMargin>
+                            </ButtonContainer>
+                        </InputContainer>
                     </CreateProgramModalContainer>
                 </Fade>
             </Modal>
