3ca75d11557c70e939eda6c0d1f1c65b1612df54
Haz-git <zhouharry1@gmail.com>
2021-01-26 10:32:39 -0800

    Created program reducer

---
+++ src/redux/userPrograms/userProgramReducer.js
@@ -0,0 +1,23 @@
+import {
+    USER_GET_EXISTING_PROGRAMS,
+    USER_ADD_NEW_PROGRAM,
+    USER_EDIT_PROGRAM,
+    USER_DELETE_PROGRAM,
+} from './userProgramTypes';
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case USER_GET_EXISTING_PROGRAMS:
+            return { ...state, programs: action.payload };
+        case USER_ADD_NEW_PROGRAM:
+            return { ...state, programs: action.payload };
+        case USER_EDIT_PROGRAM:
+            return { ...state, programs: action.payload };
+        case USER_DELETE_PROGRAM:
+            return { ...state, programs: action.payload };
+        default:
+            return state;
+    }
+};
