b15543a19a5f4942c52dc3f02e3920565503fcb8
Haz-git <zhouharry1@gmail.com>
2021-01-27 09:08:19 -0800

    Added action creators for create, edit, and deleting a new program

--- src/redux/userPrograms/userProgramActions.js
+++ src/redux/userPrograms/userProgramActions.js
@@ -16,3 +16,45 @@ export function getUserProgramData() {
         });
     };
 }
+
+export function addNewProgram(programName, programDesc) {
+    return async (dispatch) => {
+        const response = await api.post(`/user/addnewprogram`, {
+            programName,
+            programDesc,
+        });
+
+        dispatch({
+            type: USER_ADD_NEW_PROGRAM,
+            payload: response.data.userPrograms,
+        });
+    };
+}
+
+export function editExistingProgram(programId, programName, programDesc) {
+    return async (dispatch) => {
+        const response = await api.patch(`/user/editprogram`, {
+            programId,
+            programName,
+            programDesc,
+        });
+
+        dispatch({
+            type: USER_EDIT_PROGRAM,
+            payload: response.data.userPrograms,
+        });
+    };
+}
+
+export function deleteExistingProgram(programId) {
+    return async (dispatch) => {
+        const response = await api.delete(`/user/editprogram`, {
+            data: { programId },
+        });
+
+        dispatch({
+            type: USER_DELETE_PROGRAM,
+            payload: response.data.userPrograms,
+        });
+    };
+}
