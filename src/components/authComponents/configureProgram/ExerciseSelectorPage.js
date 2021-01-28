22287b48aa76b94a579700bc31d61b3d4e453507
Haz-git <zhouharry1@gmail.com>
2021-01-28 09:48:58 -0800

    Created exercise selector page due to limited size on modal

---
+++ src/components/authComponents/configureProgram/ExerciseSelectorPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+
+const ExerciseSelectorPage = () => {
+    return <div>This should be the page where you select your exercises.</div>;
+};
+
+export default ExerciseSelectorPage;
