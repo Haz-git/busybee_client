0efed72953b125349d48b9e4c95e26730e07d186
Haz-git <zhouharry1@gmail.com>
2021-02-04 06:10:01 -0800

    Small detail changes

--- src/components/authComponents/dashboardComponents/CustomSubmitButton.js
+++ src/components/authComponents/dashboardComponents/CustomSubmitButton.js
@@ -7,21 +7,21 @@ import styled from 'styled-components';
 
 const StyledSubmitButton = withStyles({
     root: {
-        fontSize: '1em',
+        fontSize: '1.2em',
         borderRadius: '2em',
         minWidth: '100%',
         maxWidth: '100%',
-        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .8)',
         background:
             'linear-gradient(90deg, rgba(87,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
         color: 'white',
-        padding: '.45em .4em',
+        padding: '.3em .2em',
         '&:hover': {
             background:
                 'linear-gradient(90deg, rgba(87,97,0,1) 0%, rgba(0,133,46,1) 50%, rgba(0,133,150,1) 100%)',
         },
         '@media only screen and (min-width: 375px)': {
-            fontSize: '1.45em',
+            fontSize: '1.25em',
         },
     },
     label: {
@@ -29,9 +29,13 @@ const StyledSubmitButton = withStyles({
     },
 })(Button);
 
-const CustomSubmitButton = ({ variant, label, type }) => {
+const CustomSubmitButton = ({ variant, label, type, clickFunc }) => {
     return (
-        <StyledSubmitButton variant={variant} type={type}>
+        <StyledSubmitButton
+            variant={variant}
+            type={type}
+            onClick={clickFunc ? clickFunc : null}
+        >
             {label}
         </StyledSubmitButton>
     );
