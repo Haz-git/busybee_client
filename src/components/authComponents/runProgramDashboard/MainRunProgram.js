b5ecb1f64e1eedfb380ae6fa9da452092fa36ce6
Haz-git <zhouharry1@gmail.com>
2021-02-05 11:14:49 -0800

    Created skeleton for run program dashboard

---
+++ src/components/authComponents/runProgramDashboard/MainRunProgram.js
@@ -0,0 +1,13 @@
+//This component should be immediately rendered after clicking 'Confirm' on program run.
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+
+const MainContainer = styled.div``;
+
+const MainRunProgram = () => {
+    return <MainContainer>The component should be running here;</MainContainer>;
+};
+
+export default MainRunProgram;
