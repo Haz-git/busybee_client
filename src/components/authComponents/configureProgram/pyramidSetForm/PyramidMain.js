a6a81f6ccb0d1731d126bc6cdf9f462af9ed5dfd
Haz-git <zhouharry1@gmail.com>
2021-02-10 11:45:29 -0800

    Created multi-step state

--- src/components/authComponents/configureProgram/pyramidSetForm/PyramidMain.js
+++ src/components/authComponents/configureProgram/pyramidSetForm/PyramidMain.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PyramidFirstStep from './PyramidFirstStep';
 import PyramidSecondStep from './PyramidSecondStep';
+import Button from '@material-ui/core/Button';
+import { v4 as uuid } from 'uuid';
 
 //Styles:
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import {
     HeaderContainer,
     MainHeader,
@@ -14,14 +16,36 @@ import {
     BackIcon,
 } from '../ConfigureMain';
 
+const carouselMovement = keyframes`
+    from {
+        opacity: 0;
+        transform: translate(20%);
+    }
+
+    to {
+        opacity: 1;
+        transform: translate(0,0);
+    }
+`;
+
 const MainContainer = styled.div`
     display: block;
     text-align: center;
 `;
 
-const FormContainer = styled.div``;
+const FormContainer = styled.div`
+    padding: 1em 1em;
+    width: 100%;
+    max-width: 100%;
+    height: 17em;
+    animation: ${carouselMovement} 0.5s linear;
+`;
 
-const ButtonContainer = styled.div``;
+const ButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
 const PyramidMain = ({
     match: {
@@ -48,6 +72,68 @@ const PyramidMain = ({
 
     */
 
+    //User Input Handlers:
+
+    const handleUserExerciseName = (e) => {
+        setExercise(e.target.value);
+    };
+
+    const handleUserSetCount = (e) => {
+        setSets(e.target.value);
+    };
+    //Click functions:
+
+    const nextFunction = () => {
+        let tempCurrentStep = currentStep;
+
+        if (tempCurrentStep === 1) {
+            setCurrentStep(tempCurrentStep + 1);
+        }
+    };
+
+    const prevFunction = () => {
+        let tempCurrentStep = currentStep;
+
+        if (tempCurrentStep === 2) {
+            setCurrentStep(tempCurrentStep - 1);
+        }
+    };
+
+    //Render Buttons:
+
+    const renderNextButton = () => {
+        // if (currentStep === 1 && exercise.trim() !== '' && sets.trim() !== '') {
+        if (currentStep === 1) {
+            return (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={nextFunction}
+                >
+                    Next
+                </Button>
+            );
+        } else {
+            return null;
+        }
+    };
+
+    const renderPreviousButton = () => {
+        if (currentStep === 2) {
+            return (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={prevFunction}
+                >
+                    Previous
+                </Button>
+            );
+        } else {
+            return null;
+        }
+    };
+
     return (
         <MainContainer>
             <HeaderContainer>
@@ -61,8 +147,25 @@ const PyramidMain = ({
                     <ExerciseHeader>Create your Pyramid</ExerciseHeader>
                 </FlexWrapper>
             </HeaderContainer>
-            <FormContainer></FormContainer>
-            <ButtonContainer></ButtonContainer>
+            <FormContainer>
+                <form>
+                    <PyramidFirstStep
+                        currentStep={currentStep}
+                        nameHandler={handleUserExerciseName}
+                        setHandler={handleUserSetCount}
+                        valueName={exercise}
+                        valueSet={sets}
+                    />
+                    <PyramidSecondStep
+                        currentStep={currentStep}
+                        valueSet={sets}
+                    />
+                </form>
+            </FormContainer>
+            <ButtonContainer>
+                {renderPreviousButton()}
+                {renderNextButton()}
+            </ButtonContainer>
         </MainContainer>
     );
 };
