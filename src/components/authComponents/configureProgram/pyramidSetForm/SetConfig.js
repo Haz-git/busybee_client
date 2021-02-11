c907ca44a781a58b2ba928e97c8ab612e8f85880
Haz-git <zhouharry1@gmail.com>
2021-02-11 08:02:13 -0800

    Added handlers as props

--- src/components/authComponents/configureProgram/pyramidSetForm/SetConfig.js
+++ src/components/authComponents/configureProgram/pyramidSetForm/SetConfig.js
@@ -14,6 +14,7 @@ const MainContainer = styled.div`
     padding: 0.5em 1.3em;
     border-radius: 1em;
     background: #1a222f;
+    margin: 1em 0;
 `;
 
 const SetLabel = styled.h2`
@@ -34,21 +35,30 @@ const FieldSpacer = styled.div`
 
 //Render:
 
-const SetConfig = ({ setNum }) => {
+const SetConfig = ({ setNum, weightHandler, repHandler }) => {
     return (
         <MainContainer>
-            <SetLabel>{setNum}</SetLabel>
+            <SetLabel>Set: {setNum}</SetLabel>
             <GridWrapper>
                 <FieldSpacer>
-                    <CustomNumberField placeholder="Weight" />
+                    <CustomNumberField
+                        placeholder="Weight"
+                        changeFunc={weightHandler}
+                    />
                 </FieldSpacer>
                 <FieldSpacer>
                     <CustomSelector />
                 </FieldSpacer>
             </GridWrapper>
-            <CustomNumberField placeholder="Reps" />
+            <CustomNumberField placeholder="Reps" changeFunc={repHandler} />
         </MainContainer>
     );
 };
+
+/*
+    I want to be able to send back the values of Weight and Reps with the corresponding setNum. So far, all I'm thinking is to add a custom props to both Custom Number fields and conditionally rendering a specialized CustomNumberField with an onInput that would have the setNum.
+
+    I also need to have a selector handler...oops.
+*/
 
 export default SetConfig;
