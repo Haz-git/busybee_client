30986bd3a898abe92d13387418dc0112c4b3342a
Haz-git <zhouharry1@gmail.com>
2021-02-16 11:15:33 -0800

    Created cards to render

---
+++ src/components/authComponents/dashboardComponents/TopProgramCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+
+//Render:
+
+const TopProgramCards = ({ name }) => {
+    return <div>{name}</div>;
+};
+
+export default TopProgramCards;
