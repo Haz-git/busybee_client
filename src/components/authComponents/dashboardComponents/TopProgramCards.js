87b116cf640159457784b8c6fd703e4c519aa31e
Haz-git <zhouharry1@gmail.com>
2021-02-16 11:51:05 -0800

    Designed and rendered top 5 program cards

--- src/components/authComponents/dashboardComponents/TopProgramCards.js
+++ src/components/authComponents/dashboardComponents/TopProgramCards.js
@@ -3,10 +3,43 @@ import React from 'react';
 //Styles:
 import styled from 'styled-components';
 
+const MainContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0.7em 0.5em;
+    box-shadow: rgba(0, 0, 0, 1) 0px 2px 5px;
+    background: #27303f;
+    border-radius: 0.3em;
+    border: none;
+    margin: 0.5em 0;
+`;
+
+const NameLabel = styled.h2`
+    font-family: 'Lato', helvetica;
+    font-size: 1em;
+    font-weight: 700;
+    color: white;
+    text-shadow: rgba(0, 0, 0, 0.3) 0px 3px 8px;
+`;
+
+const CountLabel = styled.h2`
+    font-family: 'Lato', helvetica;
+    font-size: 1em;
+    font-weight: 700;
+    color: white;
+    text-shadow: rgba(0, 0, 0, 0.3) 0px 3px 8px;
+`;
+
 //Render:
 
-const TopProgramCards = ({ name }) => {
-    return <div>{name}</div>;
+const TopProgramCards = ({ name, runCount }) => {
+    return (
+        <MainContainer>
+            <NameLabel>{name}</NameLabel>
+            <CountLabel>{runCount}</CountLabel>
+        </MainContainer>
+    );
 };
 
 export default TopProgramCards;
