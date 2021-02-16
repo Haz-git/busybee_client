3a76a405de68872a85d8a393a14837295618d071
Haz-git <zhouharry1@gmail.com>
2021-02-16 11:15:19 -0800

    Added ability to render program cards

--- src/components/authComponents/dashboardComponents/UserTopPrograms.js
+++ src/components/authComponents/dashboardComponents/UserTopPrograms.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
+import TopProgramCards from './TopProgramCards';
 
 //Styles:
 import styled from 'styled-components';
 import { MainHeader, MainContainer, StyledDivider } from './UserPowerStats';
 
-const UserTopPrograms = () => {
+const UserTopPrograms = ({ userPrograms }) => {
+    const renderTopProgramCards = () => {
+        if (userPrograms !== undefined && userPrograms.length !== 0) {
+            return userPrograms.map((program) => (
+                <TopProgramCards key={uuid()} name={program.programName} />
+            ));
+        }
+    };
+
     return (
         <MainContainer>
             <MainHeader>Your Top Programs</MainHeader>
             <StyledDivider />
+            <div>{renderTopProgramCards()}</div>
         </MainContainer>
     );
 };
