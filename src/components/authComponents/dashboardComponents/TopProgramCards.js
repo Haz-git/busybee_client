0658e5d991ebbd850517eeeffed8f530c9e8272a
Haz-git <zhouharry1@gmail.com>
2021-02-16 14:12:13 -0800

    Added exports

--- src/components/authComponents/dashboardComponents/TopProgramCards.js
+++ src/components/authComponents/dashboardComponents/TopProgramCards.js
@@ -1,34 +1,47 @@
 import React from 'react';
 
 //Styles:
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const MainContainer = styled.div`
+export const fadeIn = keyframes`
+    from {
+        transform: translateY(-10%);
+        opacity: 0;
+    }
+
+    to {
+        transform: translateY(0);
+        opacity: 1;
+    }
+`;
+
+export const MainContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 0.7em 0.5em;
-    box-shadow: rgba(0, 0, 0, 1) 0px 2px 5px;
+    box-shadow: rgba(0, 0, 0, 0.85) 0px 2px 5px;
     background: #27303f;
     border-radius: 0.3em;
     border: none;
     margin: 0.5em 0;
+    animation: ${fadeIn} 0.3s ease;
 `;
 
-const NameLabel = styled.h2`
+export const NameLabel = styled.h2`
     font-family: 'Lato', helvetica;
-    font-size: 1em;
-    font-weight: 700;
+    font-size: 0.9em;
+    font-weight: 400;
     color: white;
-    text-shadow: rgba(0, 0, 0, 0.8) 0px 1px 2px;
+    text-shadow: rgba(0, 0, 0, 1) 0px 1px 2px;
 `;
 
-const CountLabel = styled.h2`
+export const CountLabel = styled.h2`
     font-family: 'Lato', helvetica;
-    font-size: 1em;
+    font-size: 0.9em;
     font-weight: 700;
     color: white;
-    text-shadow: rgba(0, 0, 0, 0.8) 0px 1px 2px;
+    text-shadow: rgba(0, 0, 0, 1) 0px 1px 2px;
 `;
 
 //Render:
