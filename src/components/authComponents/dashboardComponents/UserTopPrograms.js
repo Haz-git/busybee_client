bd2f872fa8441e94b5ad36171d5c6a60422e0d77
Haz-git <zhouharry1@gmail.com>
2021-02-16 10:28:05 -0800

    Added box for rendering user frequently run programs

---
+++ src/components/authComponents/dashboardComponents/UserTopPrograms.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+import { MainHeader, MainContainer, StyledDivider } from './UserPowerStats';
+
+const UserTopPrograms = () => {
+    return (
+        <MainContainer>
+            <MainHeader>Your Top Programs</MainHeader>
+            <StyledDivider />
+        </MainContainer>
+    );
+};
+
+export default UserTopPrograms;
