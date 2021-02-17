409548b571a1f2cefe8e98d616918908b0bc2fa6
Haz-git <zhouharry1@gmail.com>
2021-02-16 16:39:39 -0800

    Added conditional render for no ran programs

--- src/components/authComponents/dashboardComponents/UserTopPrograms.js
+++ src/components/authComponents/dashboardComponents/UserTopPrograms.js
@@ -5,6 +5,14 @@ import TopProgramCards from './TopProgramCards';
 //Styles:
 import styled from 'styled-components';
 import { MainHeader, MainContainer, StyledDivider } from './UserPowerStats';
+import { EmotionSad } from '@styled-icons/remix-line/EmotionSad';
+
+export const SadIcon = styled(EmotionSad)`
+    height: 2.2em;
+    width: 2.2em;
+    color: white;
+    margin-bottom: 0.5em;
+`;
 
 export const LabelContainer = styled.div`
     display: flex;
@@ -16,6 +24,22 @@ export const InfoLabel = styled.h3`
     font-size: 0.8em;
     font-weight: 900;
     color: ${({ theme }) => theme.AddMoreLabelC};
+    text-shadow: rgba(0, 0, 0, 1) 0px 1px 2px;
+`;
+
+export const EmptyLabelContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const EmptyLabel = styled.h3`
+    font-size: 0.8em;
+    font-weight: 700;
+    font-family: 'Lato';
+    letter-spacing: 1px;
+    color: white;
     text-shadow: rgba(0, 0, 0, 1) 0px 1px 2px;
 `;
 
@@ -36,6 +60,7 @@ const UserTopPrograms = ({ userPrograms }) => {
     //Render top 5 objects in array into cards -->
 
     const renderTopProgramCards = () => {
+        console.log(sortedArray);
         if (sortedArray !== undefined && sortedArray.length !== 0) {
             return sortedArray
                 .slice(0, 5)
@@ -46,6 +71,15 @@ const UserTopPrograms = ({ userPrograms }) => {
                         runCount={program.runCount}
                     />
                 ));
+        } else {
+            return (
+                <EmptyLabelContainer>
+                    <SadIcon />
+                    <EmptyLabel>
+                        Looks like you haven't ran a program yet.
+                    </EmptyLabel>
+                </EmptyLabelContainer>
+            );
         }
     };
 
