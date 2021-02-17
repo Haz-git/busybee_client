aa4a2619d9d65f7d82dc492534c7abeb5f8bde5c
Haz-git <zhouharry1@gmail.com>
2021-02-17 09:55:36 -0800

    Testing new settings modal

---
+++ src/components/authComponents/settingsDashboard/SettingsModal.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+import {
+    ModalContainer,
+    ModalHeader,
+    ModalDesc,
+} from '../dashboardComponents/UserPowerStatCard';
+
+const SettingsModalContainer = styled(ModalContainer)`
+    width: 92.5%;
+`;
+
+const SettingsModalHeader = styled(ModalHeader)`
+    white-space: normal;
+`;
+
+const SettingsModalDesc = styled(ModalDesc)`
+    margin: 1em 0;
+    white-space: normal;
+    text-align: center;
+`;
+
+const ButtonContainer = styled.div`
+    margin-top: 1em;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+const SettingsModal = ({
+    openBoolean,
+    closeFunction,
+    buttonSubmitFunction,
+    modalHeader,
+    modalDesc,
+    ariaLabel,
+    ariaDesc,
+}) => {
+    return (
+        <>
+            <Modal
+                aria-labelledby={ariaLabel}
+                aria-describedby={ariaDesc}
+                open={openBoolean}
+                onClose={closeFunction}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openBoolean}>
+                    <SettingsModalContainer></SettingsModalContainer>
+                </Fade>
+            </Modal>
+        </>
+    );
+};
