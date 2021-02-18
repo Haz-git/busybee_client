05946843800c6f76ce2d4640b3a810218345539f
Haz-git <zhouharry1@gmail.com>
2021-02-18 10:22:04 -0800

    Updated reducer with new types

--- src/redux/userDetails/detailReducer.js
+++ src/redux/userDetails/detailReducer.js
@@ -1,10 +1,21 @@
-import { USER_GET_EXISTING_DETAILS } from './detailTypes';
+import {
+    USER_GET_EXISTING_DETAILS,
+    USER_EDIT_GENERAL_INFO,
+    USER_EDIT_EMAIL,
+    USER_EDIT_PASSWORD,
+} from './detailTypes';
 
 const initialState = {};
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case USER_GET_EXISTING_DETAILS:
+            return { ...state, user: action.payload };
+        case USER_EDIT_GENERAL_INFO:
+            return { ...state, user: action.payload };
+        case USER_EDIT_EMAIL:
+            return { ...state, user: action.payload };
+        case USER_EDIT_PASSWORD:
             return { ...state, user: action.payload };
         default:
             return state;
