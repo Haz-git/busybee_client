70645bbab5b7e69fea12a1348d7e37c058c7102a
Haz-git <zhouharry1@gmail.com>
2021-02-18 10:21:34 -0800

    Added three controller functions

--- src/redux/userDetails/detailActions.js
+++ src/redux/userDetails/detailActions.js
@@ -1,5 +1,10 @@
 import api from '../../api';
-import { USER_GET_EXISTING_DETAILS } from './detailTypes';
+import {
+    USER_GET_EXISTING_DETAILS,
+    USER_EDIT_GENERAL_INFO,
+    USER_EDIT_EMAIL,
+    USER_EDIT_PASSWORD,
+} from './detailTypes';
 
 export function getUserExistingDetails() {
     return async (dispatch) => {
@@ -13,5 +18,54 @@ export function getUserExistingDetails() {
         if (response) {
             return true;
         }
+    };
+}
+
+//Although it is possible to write a single dispatch function for all user detail edits, three are created below for increased readability.
+
+export function userEditGeneralInfo(
+    type,
+    newUserName,
+    newFirstName,
+    newLastName
+) {
+    return async (dispatch) => {
+        const response = await api.post('/user/settings/edituserdetails', {
+            type,
+            newUserName,
+            newFirstName,
+            newLastName,
+        });
+
+        console.log(response);
+    };
+}
+
+export function userEditEmail(type, newEmail) {
+    return async (dispatch) => {
+        const response = await api.post('/user/settings/edituserdetails', {
+            type,
+            newEmail,
+        });
+
+        console.log(response);
+    };
+}
+
+export function userEditPassword(
+    type,
+    newPassword,
+    newPasswordConfirm,
+    currentPassword
+) {
+    return async (dispatch) => {
+        const response = await api.post('/user/settings/edituserdetails', {
+            type,
+            newPassword,
+            newPasswordConfirm,
+            currentPassword,
+        });
+
+        console.log(response);
     };
 }
