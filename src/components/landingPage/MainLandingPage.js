cc3be6d0e10985721d42a2c307a181e100ff5886
Haz-git <zhouharry1@gmail.com>
2021-02-19 14:08:52 -0800

    Redesigned landing page

--- src/components/landingPage/MainLandingPage.js
+++ src/components/landingPage/MainLandingPage.js
@@ -11,8 +11,55 @@ import history from '../historyObject';
 import gymjot_logo from '../../imgs/gymjot_transparent.png';
 
 //Styles:
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Button from '@material-ui/core/Button';
+
+const rotateAnimate = keyframes`
+    from {
+        transform: rotate(90deg);
+        -webkit-transform: rotate(90deg);
+        opacity: .1;
+
+    }
+
+    to {
+        transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        opacity: 1;
+    }
+`;
+
+const fadeIn = keyframes`
+    from {
+        transform: translateY(-20%);
+        opacity: 0;
+    }
+
+    to {
+        transform: translateY(0);
+        opacity: 1;
+    }
+`;
+
+const moveInLogin = keyframes`
+    from {
+        transform: translateY(80%);
+    }
+
+    to {
+        transform: translateY(0);
+    }
+`;
+
+const moveInJoin = keyframes`
+    from {
+        transform: translateY(120%);
+    }
+
+    to {
+        transform: translateY(0);
+    }
+`;
 
 const MobMainContainer = styled.div`
     background: ${(props) => props.theme.background};
@@ -21,7 +68,7 @@ const MobMainContainer = styled.div`
 
 const MobWrapper = styled.div`
     position: relative;
-    top: 45%;
+    top: 50%;
     transform: translateY(-50%);
     text-align: center;
 `;
@@ -32,35 +79,80 @@ const LogoContainer = styled.div``;
 
 export const StyledMainLogo = styled.img`
     object-fit: cover;
-    height: 10em;
-    width: 10em;
+    height: 14em;
+    width: 14em;
     border-radius: 50%;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    animation: ${rotateAnimate} 1s ease;
 `;
 
 const MobHeader = styled.h1`
     font-family: 'Nunito', sans-serif, helvetica;
     font-size: 4em;
-    font-weight: 100;
+    font-weight: 900;
     color: ${(props) => props.theme.mainMobHeaderColor};
+    animation: ${fadeIn} 0.5s ease;
 `;
 
 const MobLabel = styled.label`
     font-family: 'Nunito', sans-serif, helvetica;
     font-size: 1.2em;
-    font-weight: 100;
+    font-weight: 900;
     color: ${(props) => props.theme.mainMobHeaderColor};
+    animation: ${fadeIn} 0.5s ease;
 `;
 
 const MobButtonContainer = styled.div`
-    padding: 1em 0;
+    padding: 1em 1em;
     display: flex;
-    align-items: center;
+    flex-direction: column;
     justify-content: center;
 `;
 
 const MobButtonDiv = styled.div`
-    margin: 3em 0.8em;
+    margin: 0.5em 0;
+`;
+
+const CustomLoginButton = styled.button`
+    color: white;
+    max-width: 100%;
+    min-width: 100%;
+    background: #156711;
+    border-radius: 2em;
+    font-family: 'Nunito', sans-serif, helvetica;
+    font-weight: 300;
+    font-size: 1.2em;
+    letter-spacing: 0.1em;
+    padding: 0.8em 0;
+    border: none;
+    text-shadow: rgba(0, 0, 0, 1) 0px 3px 4px;
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 5px 5px;
+    animation: ${moveInLogin} 0.5s ease;
+
+    &:hover {
+        background: #14a84b;
+    }
+`;
+
+const CustomJoinButton = styled.button`
+    color: white;
+    max-width: 100%;
+    min-width: 100%;
+    background: #27303f;
+    border-radius: 2em;
+    font-family: 'Nunito', sans-serif, helvetica;
+    font-weight: 300;
+    font-size: 1.2em;
+    letter-spacing: 0.1em;
+    padding: 0.8em 0;
+    border: none;
+    text-shadow: rgba(0, 0, 0, 1) 0px 3px 4px;
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 5px 5px;
+    animation: ${moveInJoin} 0.5s ease;
+
+    &:hover {
+        background: #5c677b;
+    }
 `;
 
 //Render:
@@ -94,24 +186,16 @@ const MainLandingPage = () => {
                             <MobButtonContainer>
                                 <MobButtonDiv>
                                     <Link to="/login">
-                                        <Button
-                                            variant="contained"
-                                            size="large"
-                                            color="primary"
-                                        >
-                                            Login
-                                        </Button>
+                                        <CustomLoginButton>
+                                            Sign In
+                                        </CustomLoginButton>
                                     </Link>
                                 </MobButtonDiv>
                                 <MobButtonDiv>
                                     <Link to="/signup">
-                                        <Button
-                                            variant="outlined"
-                                            size="large"
-                                            color="primary"
-                                        >
+                                        <CustomJoinButton>
                                             Join Us
-                                        </Button>
+                                        </CustomJoinButton>
                                     </Link>
                                 </MobButtonDiv>
                             </MobButtonContainer>
