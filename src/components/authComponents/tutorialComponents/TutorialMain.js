35cb0e6b225fff530f8f1d2f598cd8626a551d78
Haz-git <zhouharry1@gmail.com>
2021-02-24 11:00:14 -0800

    Added return to main button for escape testing

--- src/components/authComponents/tutorialComponents/TutorialMain.js
+++ src/components/authComponents/tutorialComponents/TutorialMain.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import historyObject from '../../historyObject';
 
 import styled from 'styled-components';
 
@@ -16,6 +18,14 @@ const TutorialMain = () => {
         <MainContainer>
             <MainText>Tutorial Page</MainText>
             <MainText>Under Construction~~</MainText>
+            <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                onClick={() => historyObject.push('/')}
+            >
+                Return to Dashboard
+            </Button>
         </MainContainer>
     );
 };
