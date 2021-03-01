f20b7cd8538dc74c624ec9bc7d683a5aa0adc0c5
Haz-git <zhouharry1@gmail.com>
2021-03-01 10:03:26 -0800

    Created modal for cardio stat selection

---
+++ src/components/authComponents/configureProgram/CardioSelectModal.js
@@ -0,0 +1,140 @@
+import React from 'react';
+
+//Styles:
+import styled from 'styled-components';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import CustomNumberField from '../dashboardComponents/CustomNumberField';
+import CustomTextField from '../dashboardComponents/CustomTextField';
+import CustomSelector from '../dashboardComponents/CustomSelector';
+import { withStyles } from '@material-ui/core/styles';
+
+import {
+    ModalContainer,
+    ModalHeader,
+    ModalDesc,
+} from '../dashboardComponents/UserPowerStatCard';
+
+const CardioModalContainer = styled(ModalContainer)`
+    width: 92%;
+`;
+
+const CardioModalHeader = styled(ModalHeader)`
+    font-size: 1.8em;
+    font-weight: 900;
+    margin: 0.5em 0;
+`;
+
+const TextFieldDivider = styled.div`
+    margin-top: 0.5em;
+    margin-bottom: 0.8em;
+`;
+
+const InputContainer = styled.div`
+    width: 100%;
+    max-width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 0.7em;
+`;
+
+const InputDivider = styled.div`
+    margin: 0.3em 0.3em;
+`;
+
+const FieldDivider = styled.div`
+    margin: 0.8em 0;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+const StyledButton = withStyles({
+    root: {
+        textTransform: 'capitalize',
+        fontSize: '1.3em',
+    },
+})(Button);
+
+const CardioSelectModal = ({
+    ariaLab,
+    ariaDesc,
+    modalHeader,
+    openBoolean,
+    closeFunction,
+    submitHandler,
+    nameFunction,
+    maxTextLength,
+    minHandler,
+    secHandler,
+}) => {
+    return (
+        <>
+            <Modal
+                aria-labelledby={ariaLab}
+                aria-describedby={ariaDesc}
+                open={openBoolean}
+                onClose={closeFunction}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openBoolean}>
+                    <CardioModalContainer>
+                        <CardioModalHeader>{modalHeader}</CardioModalHeader>
+                        <TextFieldDivider>
+                            <CustomTextField
+                                type="text"
+                                placeholder="Cardio exercise..."
+                                changeFunc={nameFunction}
+                                maxlength={maxTextLength}
+                            />
+                        </TextFieldDivider>
+                        <InputContainer>
+                            <InputDivider>
+                                <CustomNumberField
+                                    placeholder="Minutes"
+                                    changeFunc={minHandler}
+                                />
+                            </InputDivider>
+                            <InputDivider>
+                                <CustomNumberField
+                                    placeholder="Seconds"
+                                    changeFunc={secHandler}
+                                />
+                            </InputDivider>
+                        </InputContainer>
+                        <ButtonContainer>
+                            <StyledButton
+                                size="large"
+                                variant="contained"
+                                color="primary"
+                                onClick={submitHandler}
+                            >
+                                Save
+                            </StyledButton>
+                            <StyledButton
+                                size="large"
+                                variant="contained"
+                                color="secondary"
+                                onClick={closeFunction}
+                            >
+                                Cancel
+                            </StyledButton>
+                        </ButtonContainer>
+                    </CardioModalContainer>
+                </Fade>
+            </Modal>
+        </>
+    );
+};
+
+export default CardioSelectModal;
