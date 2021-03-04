a58aa4eb614ec0dd14ebdb7ed5a1d5cbc5e18d7b
Haz-git <zhouharry1@gmail.com>
2021-03-04 15:31:33 -0800

    Added desktop only styles for login form

--- src/components/loginPage/MainloginForm.js
+++ src/components/loginPage/MainloginForm.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
+
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile,
+} from 'react-device-detect';
+
 import LoadingPage from './LoadingPage';
 import userLogin from '../../redux/userLogin/userLoginActions';
 import InputField from '../signupPage/InputField';
@@ -49,6 +57,26 @@ const WrapperContainer = styled.div`
     }
 `;
 
+const DeskWrapperContainer = styled.div`
+    position: relative;
+    top: 50vh;
+    -webkit-transform: translateY(-30vh);
+    -moz-transform: translateY(-30vh);
+    -ms-transform: translateY(-30vh);
+    -o-transform: translateY(-30vh);
+    transform: translateY(-30vh);
+    text-align: center;
+`;
+
+const DeskShadowContainer = styled.div`
+    box-shadow: rgba(0, 0, 0, 0.8) 0px 3px 4px;
+    max-width: 40em;
+    width: 40em;
+    margin: 0 auto;
+    padding: 2em 2em;
+    border-radius: 1em;
+`;
+
 const LogoContainer = styled(Link)`
     text-align: center;
 `;
@@ -65,7 +93,19 @@ const StyledLoginHeader = styled.h1`
     }
 `;
 
+const DeskLoginHeader = styled.h1`
+    color: ${({ theme }) => theme.MobHeaderColor};
+    font-size: 2em;
+    font-family: 'Nunito', sans-serif, helvetica;
+    font-weight: 900;
+    margin-top: 0.5em;
+`;
+
 const FormContainer = styled.div`
+    padding: 1em 2em;
+`;
+
+const DeskFormContainer = styled.div`
     padding: 1em 2em;
 `;
 
@@ -93,6 +133,14 @@ const ErrorTextInvisible = styled.h2`
     }
 `;
 
+const DeskErrorTextInvisible = styled.h2`
+    font-family: 'Nunito', sans-serif;
+    color: red;
+    font-size: 1.35em;
+    font-weight: 600;
+    opacity: 0;
+`;
+
 const ErrorTextVisible = styled.h2`
     font-family: 'Nunito', sans-serif;
     color: red;
@@ -110,6 +158,13 @@ const ErrorTextVisible = styled.h2`
     @media only screen and (max-width: 850px) and (orientation: landscape) {
         font-size: 1em;
     }
+`;
+
+const DeskErrorTextVisible = styled.h2`
+    font-family: 'Nunito', sans-serif;
+    color: red;
+    font-size: 1.35em;
+    font-weight: 600;
 `;
 
 //Render:
@@ -145,65 +200,151 @@ const MainLoginForm = ({ handleSubmit, userLogin }) => {
     const renderErrorText = () => {
         //This function renders the same verification error text, but with different opacities according to the hasErrors state.
         if (hasErrors === true) {
-            return (
-                <Fade>
-                    <ErrorTextVisible>
-                        Your verification details were incorrect.
-                    </ErrorTextVisible>
-                </Fade>
-            );
+            if (isMobile) {
+                return (
+                    <Fade>
+                        <ErrorTextVisible>
+                            Your verification details were incorrect.
+                        </ErrorTextVisible>
+                    </Fade>
+                );
+            } else if (isBrowser) {
+                return (
+                    <Fade>
+                        <DeskErrorTextVisible>
+                            Your verification details were incorrect.
+                        </DeskErrorTextVisible>
+                    </Fade>
+                );
+            }
         } else {
+            if (isMobile) {
+                return (
+                    <Fade>
+                        <ErrorTextInvisible>
+                            Your verification details were incorrect.
+                        </ErrorTextInvisible>
+                    </Fade>
+                );
+            } else if (isBrowser) {
+                return (
+                    <Fade>
+                        <DeskErrorTextInvisible>
+                            Your verification details were incorrect.
+                        </DeskErrorTextInvisible>
+                    </Fade>
+                );
+            }
+        }
+    };
+
+    const renderMainLoginForm = () => {
+        if (isMobile) {
             return (
-                <ErrorTextInvisible>
-                    Your verification details were incorrect.
-                </ErrorTextInvisible>
+                <MobileView>
+                    <LoadingPage renderLoading={renderLoading} />
+                    <MainContainer>
+                        <WrapperContainer>
+                            <LogoContainer to="/">
+                                <LoginLogo
+                                    src={gymjot_logo}
+                                    alt="gymjot logo"
+                                />
+                            </LogoContainer>
+                            <StyledLoginHeader>
+                                Welcome back, GymJotter.
+                            </StyledLoginHeader>
+                            <form onSubmit={handleSubmit(handleUserLogin)}>
+                                <FormContainer>
+                                    {renderErrorText()}
+                                    <InputField
+                                        formName="email"
+                                        componentType="input"
+                                        label="Email Address"
+                                        htmlType="email"
+                                    >
+                                        <StyledUserIcon />
+                                    </InputField>
+                                    <InputField
+                                        formName="password"
+                                        componentType="input"
+                                        label="Password"
+                                        htmlType="password"
+                                    >
+                                        <StyledLockIcon />
+                                    </InputField>
+                                    <ButtonContainer>
+                                        <SubmitButton label="Login" />
+                                    </ButtonContainer>
+                                </FormContainer>
+                            </form>
+                            <div>
+                                <PromptSpan>
+                                    Don't have an account yet?
+                                </PromptSpan>
+                                <PromptLink to="/signup">Join here.</PromptLink>
+                            </div>
+                        </WrapperContainer>
+                    </MainContainer>
+                </MobileView>
+            );
+        } else if (isBrowser) {
+            return (
+                <BrowserView>
+                    <LoadingPage renderLoading={renderLoading} />
+                    <MainContainer>
+                        <DeskWrapperContainer>
+                            <DeskShadowContainer>
+                                <LogoContainer to="/">
+                                    <LoginLogo
+                                        src={gymjot_logo}
+                                        alt="gymjot logo"
+                                    />
+                                </LogoContainer>
+                                <DeskLoginHeader>
+                                    Welcome back, GymJotter.
+                                </DeskLoginHeader>
+                                <form onSubmit={handleSubmit(handleUserLogin)}>
+                                    <DeskFormContainer>
+                                        {renderErrorText()}
+                                        <InputField
+                                            formName="email"
+                                            componentType="input"
+                                            label="Email Address"
+                                            htmlType="email"
+                                        >
+                                            <StyledUserIcon />
+                                        </InputField>
+                                        <InputField
+                                            formName="password"
+                                            componentType="input"
+                                            label="Password"
+                                            htmlType="password"
+                                        >
+                                            <StyledLockIcon />
+                                        </InputField>
+                                        <ButtonContainer>
+                                            <SubmitButton label="Login" />
+                                        </ButtonContainer>
+                                    </DeskFormContainer>
+                                </form>
+                                <div>
+                                    <PromptSpan>
+                                        Don't have an account yet?
+                                    </PromptSpan>
+                                    <PromptLink to="/signup">
+                                        Join here.
+                                    </PromptLink>
+                                </div>
+                            </DeskShadowContainer>
+                        </DeskWrapperContainer>
+                    </MainContainer>
+                </BrowserView>
             );
         }
     };
 
-    return (
-        <>
-            <LoadingPage renderLoading={renderLoading} />
-            <MainContainer>
-                <WrapperContainer>
-                    <LogoContainer to="/">
-                        <LoginLogo src={gymjot_logo} alt="gymjot logo" />
-                    </LogoContainer>
-                    <StyledLoginHeader>
-                        Welcome back, GymJotter.
-                    </StyledLoginHeader>
-                    <form onSubmit={handleSubmit(handleUserLogin)}>
-                        <FormContainer>
-                            {renderErrorText()}
-                            <InputField
-                                formName="email"
-                                componentType="input"
-                                label="Email Address"
-                                htmlType="email"
-                            >
-                                <StyledUserIcon />
-                            </InputField>
-                            <InputField
-                                formName="password"
-                                componentType="input"
-                                label="Password"
-                                htmlType="password"
-                            >
-                                <StyledLockIcon />
-                            </InputField>
-                            <ButtonContainer>
-                                <SubmitButton label="Login" />
-                            </ButtonContainer>
-                        </FormContainer>
-                    </form>
-                    <div>
-                        <PromptSpan>Don't have an account yet?</PromptSpan>
-                        <PromptLink to="/signup">Join here.</PromptLink>
-                    </div>
-                </WrapperContainer>
-            </MainContainer>
-        </>
-    );
+    return <>{renderMainLoginForm()}</>;
 };
 
 //Connecting to Redux and Redux-form:
