71b136486569bf8871ae87bd06a13366598bf494
Haz-git <zhouharry1@gmail.com>
2021-03-10 10:09:23 -0800

    Browser support for Settings Cards

--- src/components/authComponents/settingsDashboard/SettingsCard.js
+++ src/components/authComponents/settingsDashboard/SettingsCard.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+    BrowserView,
+    MobileOnlyView,
+    isBrowser,
+    isMobileOnly,
+} from 'react-device-detect';
 
 //Styles:
 import styled from 'styled-components';
@@ -18,6 +24,37 @@ const MainContainer = styled.button`
     background: #1a222f;
     cursor: pointer;
     transition: 0.1s linear;
+
+    &:hover {
+        transform: scale(1.1);
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    @media screen and (min-width: 414px) {
+        height: 14em;
+        width: 14em;
+    }
+`;
+
+const BrowserMainContainer = styled.button`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 0.4em;
+    /* width: 4em; */
+    width: 12em;
+    height: 12em;
+    border: 1px solid #fdbc3d;
+    box-shadow: rgba(0, 0, 0, 0.45) 0px 3px 8px;
+    padding: 0.8em 0.8em;
+    background: #1a222f;
+    cursor: pointer;
+    transition: 0.1s linear;
+    margin: 0 1em;
 
     &:hover {
         transform: scale(1.1);
@@ -53,12 +90,24 @@ const TextLabel = styled.h2`
 
 const SettingsCard = ({ icon, textLabel, clickFunc }) => {
     return (
-        <MainContainer onClick={clickFunc}>
-            <IconContainer>{icon}</IconContainer>
-            <TextContainer>
-                <TextLabel>{textLabel}</TextLabel>
-            </TextContainer>
-        </MainContainer>
+        <>
+            {isMobileOnly && (
+                <MainContainer onClick={clickFunc}>
+                    <IconContainer>{icon}</IconContainer>
+                    <TextContainer>
+                        <TextLabel>{textLabel}</TextLabel>
+                    </TextContainer>
+                </MainContainer>
+            )}
+            {isBrowser && (
+                <BrowserMainContainer onClick={clickFunc}>
+                    <IconContainer>{icon}</IconContainer>
+                    <TextContainer>
+                        <TextLabel>{textLabel}</TextLabel>
+                    </TextContainer>
+                </BrowserMainContainer>
+            )}
+        </>
     );
 };
 
