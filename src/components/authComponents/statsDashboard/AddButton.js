d0aa86fef894e39a55ee1fba40d647d55065c6c9
Haz-git <zhouharry1@gmail.com>
2021-03-23 09:47:12 -0700

    Minor styling changes

--- src/components/authComponents/statsDashboard/AddButton.js
+++ src/components/authComponents/statsDashboard/AddButton.js
@@ -9,20 +9,23 @@ import { AddCircle } from '@styled-icons/ionicons-solid/AddCircle';
 const AddIcon = styled(AddCircle)`
     height: 1.1em;
     width: 1.1em;
+    margin-bottom: 0.1em;
 `;
 
 const CustomButton = withStyles({
     root: {
+        display: 'flex',
+        alignItems: 'center',
         boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
         backgroundColor: '#27303f',
         textTransform: 'capitalize',
         color: '#fdbc3d',
         fontFamily: 'Nunito, Lato, sans-serif',
-        fontWeight: '400',
-        fontSize: '1.2em',
+        fontWeight: '700',
+        fontSize: '1.175em',
         padding: '.5em .8em',
         borderRadius: '.3em',
-        marginLeft: '.4em',
+        marginLeft: '.5em',
     },
 })(Button);
 
@@ -32,7 +35,7 @@ const AddButton = ({ clickFunction }) => {
     return (
         <>
             <CustomButton startIcon={<AddIcon />} onClick={clickFunction}>
-                Add
+                New
             </CustomButton>
         </>
     );
