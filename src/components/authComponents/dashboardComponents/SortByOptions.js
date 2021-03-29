0fc30e53ceb609df5f1259baab990ca1e856bc5c
Haz-git <zhouharry1@gmail.com>
2021-03-29 06:11:56 -0700

    Created new sort component for stat sorting

---
+++ src/components/authComponents/dashboardComponents/SortByOptions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled.div``;
+
+const SortByOptions = () => {
+    return (
+        <>
+            <MainContainer></MainContainer>
+        </>
+    );
+};
+
+export default SortByOptions;
