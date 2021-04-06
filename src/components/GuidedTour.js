921d09b1ff109334f9c251212e5f8171c04cb52a
Haz-git <zhouharry1@gmail.com>
2021-04-06 16:02:37 -0700

    Created HOC GuidedTour using reactour

--- src/components/GuidedTour.js
+++ src/components/GuidedTour.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import Tour from 'reactour';
+import historyObject from './historyObject';
 
-const GuidedTour = () => {
-    const TOUR_STEPS = [
+const GuidedTour = ({ openOn, closeFunc }) => {
+    //States for moving around the app. Unfortunately, using the history object with reactour generates an infinite loop. We'll first check if we have navigated to an area before using the historyObject.
+
+    const [hasNavigatedToSettings, setHasNavigatedToSettings] = useState(false);
+
+    const disableBody = (target) => disableBodyScroll(target);
+    const enableBody = (target) => enableBodyScroll(target);
+
+    const steps = [
         {
-            target: '.tester-mainheader',
-            content: 'This is our tour’s logo',
+            selector: '.UserPowerStats-StatCardContainer',
+            content: `Here's your current primary lifts. You don't have any yet, but after this tutorial feel free to use 'edit' to add a new lift!`,
+        },
+        {
+            content: 'Settings Menu',
+            action: () => {
+                if (!hasNavigatedToSettings) {
+                    historyObject.push('/settings');
+                    setHasNavigatedToSettings(true);
+                }
+            },
+        },
+        {
+            selector: '.MainSettings-MainHeader',
+            content: 'Test',
         },
     ];
-    return <></>;
+
+    return (
+        <>
+            <Tour
+                steps={steps}
+                isOpen={openOn}
+                onRequestClose={() => closeFunc(false)}
+                rounded={10}
+                onAfterOpen={disableBody}
+                onBeforeClose={enableBody}
+                disableInteraction={true}
+                disableKeyboardNavigation={true}
+                showNumber={false}
+                maskSpace={10}
+                closeWithMask={false}
+            />
+        </>
+    );
 };
 
 export default GuidedTour;
