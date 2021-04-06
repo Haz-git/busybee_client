9dd18b7e9cb69ed0d233c8240e17f6ddf3477bc4
Haz-git <zhouharry1@gmail.com>
2021-04-06 16:46:42 -0700

    Trying to implement routing with tour...

--- src/components/GuidedTour.js
+++ src/components/GuidedTour.js
@@ -1,53 +1,68 @@
 import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import Tour from 'reactour';
 import historyObject from './historyObject';
 
-const GuidedTour = ({ openOn, closeFunc }) => {
-    //States for moving around the app. Unfortunately, using the history object with reactour generates an infinite loop. We'll first check if we have navigated to an area before using the historyObject.
+const GuidedTour = withRouter(
+    ({ openOn, closeFunc, location: { pathname } }) => {
+        //States for moving around the app. Unfortunately, using the history object with reactour generates an infinite loop. We'll first check if we have navigated to an area before using the historyObject.
 
-    const [hasNavigatedToSettings, setHasNavigatedToSettings] = useState(false);
+        const [hasNavigatedToSettings, setHasNavigatedToSettings] = useState(
+            false
+        );
 
-    const disableBody = (target) => disableBodyScroll(target);
-    const enableBody = (target) => enableBodyScroll(target);
+        console.log(pathname);
 
-    const steps = [
-        {
-            selector: '.UserPowerStats-StatCardContainer',
-            content: `Here's your current primary lifts. You don't have any yet, but after this tutorial feel free to use 'edit' to add a new lift!`,
-        },
-        {
-            content: 'Settings Menu',
-            action: () => {
-                if (!hasNavigatedToSettings) {
-                    historyObject.push('/settings');
-                    setHasNavigatedToSettings(true);
-                }
+        const disableBody = (target) => disableBodyScroll(target);
+        const enableBody = (target) => enableBodyScroll(target);
+
+        const steps = [
+            {
+                content: 'Welcome to the GymJot Walkthrough!',
             },
-        },
-        {
-            selector: '.MainSettings-MainHeader',
-            content: 'Test',
-        },
-    ];
+            ...(pathname === '/dashboard'
+                ? [
+                      {
+                          selector: '.UserPowerStats-StatCardContainer',
+                          content: `Check the status of your primary lifts at a glance.`,
+                      },
+                      {
+                          content: 'Settings Menu',
+                          action: () => historyObject.push('/settings'),
+                      },
+                  ]
+                : []),
+            ...(pathname === '/settings'
+                ? [
+                      {
+                          selector: '.MainSettings-MainHeader',
+                          content: 'Test',
+                      },
+                  ]
+                : []),
+        ];
 
-    return (
-        <>
-            <Tour
-                steps={steps}
-                isOpen={openOn}
-                onRequestClose={() => closeFunc(false)}
-                rounded={10}
-                onAfterOpen={disableBody}
-                onBeforeClose={enableBody}
-                disableInteraction={true}
-                disableKeyboardNavigation={true}
-                showNumber={false}
-                maskSpace={10}
-                closeWithMask={false}
-            />
-        </>
-    );
-};
+        return (
+            <>
+                <Tour
+                    steps={steps}
+                    isOpen={openOn}
+                    onRequestClose={() => closeFunc(false)}
+                    rounded={10}
+                    onAfterOpen={disableBody}
+                    onBeforeClose={enableBody}
+                    disableInteraction={true}
+                    disableKeyboardNavigation={true}
+                    showNumber={false}
+                    maskSpace={10}
+                    closeWithMask={false}
+                    showNavigation={true}
+                    update={pathname}
+                />
+            </>
+        );
+    }
+);
 
 export default GuidedTour;
