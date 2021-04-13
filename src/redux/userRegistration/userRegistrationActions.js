80156e1360c8605ae568b501f9a603a4ade68539
Haz-git <zhouharry1@gmail.com>
2021-04-13 05:58:18 -0700

    Attached button callback

--- src/redux/userRegistration/userRegistrationActions.js
+++ src/redux/userRegistration/userRegistrationActions.js
@@ -2,16 +2,21 @@ import api from '../../api';
 import history from '../../components/historyObject';
 import { USER_REGISTRATION } from './userRegistrationTypes';
 
-const userRegistration = (formValues, callback) => async (dispatch) => {
+const userRegistration = (
+    formValues,
+    snackbarCallback,
+    buttonCallback
+) => async (dispatch) => {
     //Sending redux-form values to Atlas:
 
     const response = await api.post('/user/signup', { ...formValues });
 
     //There is currently no dispatch for this function in Redux. Perhaps later we will need something like that.
 
-    if (response) {
+    if (response && snackbarCallback && buttonCallback) {
         //if response.ok === true, then the user's account has been created.
-        callback(true);
+        snackbarCallback(true);
+        buttonCallback(false);
     }
 };
 
