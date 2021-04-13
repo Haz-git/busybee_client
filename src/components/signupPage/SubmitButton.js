f031c57e9d1112009d75b5f1c8841a2040ddc27b
Haz-git <zhouharry1@gmail.com>
2021-04-13 05:57:57 -0700

    Added disabled state handler and spinner

--- src/components/signupPage/SubmitButton.js
+++ src/components/signupPage/SubmitButton.js
@@ -8,6 +8,9 @@ import Button from '@material-ui/core/Button';
 //Styles:
 const CustomButton = withStyles({
     root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
         color: '#ffffff',
         maxWidth: '100%',
         minWidth: '100%',
@@ -15,12 +18,17 @@ const CustomButton = withStyles({
         borderRadius: '2em',
         fontFamily: 'Nunito, sans-serif, helvetica,',
         fontWeight: '300',
-        fontSize: '.6em',
+        fontSize: '.8em',
         letterSpacing: '.1em',
         padding: '1em 0',
         textTransform: 'capitalize',
+        transition: 'all .2s ease-in-out',
         '&:hover': {
             backgroundColor: '#62c267',
+        },
+        '&:disabled': {
+            color: 'white',
+            backgroundColor: '#1a222f',
         },
         '@media only screen and (min-width: 375px)': {
             padding: '.7em 0',
@@ -30,9 +38,15 @@ const CustomButton = withStyles({
 })(Button);
 
 //Render:
-const SubmitButton = ({ label, formID }) => {
+const SubmitButton = ({ label, formID, loader, disabledState }) => {
     return (
-        <CustomButton type="submit" value="submit" form={formID}>
+        <CustomButton
+            type="submit"
+            value="submit"
+            form={formID}
+            disabled={disabledState}
+        >
+            {loader}
             {label}
         </CustomButton>
     );
